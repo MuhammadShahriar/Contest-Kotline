168b620773989b28c6e7f8d5ab9a53eee6b8c71c
MuhammadShahriar <37985651+MuhammadShahriar@users.noreply.github.com>
2021-10-04 21:51:52 +0600

    template changed

--- Kotlin Heroes Practice 8/C.kt
+++ Kotlin Heroes Practice 8/C.kt
@@ -1,22 +1,80 @@
-///In the name of ALLAH
+import java.io.PrintWriter
+import java.util.*
+import kotlin.collections.ArrayDeque
 
-import java.lang.AssertionError
- 
-private fun readLn() = readLine()!! // string line
-private fun readInt() = readLn().toInt() // single int
-private fun readLong() = readLn().toLong() // single long
-private fun readDouble() = readLn().toDouble() // single double
-private fun readStrings() = readLn().split(" ") // list of strings
-private fun readInts() = readStrings().map { it.toInt() } // list of ints
-private fun readLongs() = readStrings().map { it.toLong() } // list of longs
-private fun readDoubles() = readStrings().map { it.toDouble() } // list of doubles
- 
-private fun myAssert(x: Boolean) {
-    if (!x) {
-        throw AssertionError()
-    }
+val pw = PrintWriter(System.out)
+fun main() {
+    _writer.solve(); _writer.flush()
 }
 
-fun main(args: Array<String>) {
+fun PrintWriter.solve() {
+    var n = readInt();
+    var a = IntArray(5);
+    a = readAInt ( n );
     
+    for (i in 0..n-1) println(a[i]);
 }
+
+@JvmField
+val INPUT = System.`in`
+
+@JvmField
+val OUTPUT = System.out
+
+@JvmField
+val _reader = INPUT.bufferedReader()
+fun readLine(): String? = _reader.readLine()
+fun readLn() = _reader.readLine()!!
+
+@JvmField
+var _tokenizer: StringTokenizer = StringTokenizer("")
+fun _read(): String {
+    while (_tokenizer.hasMoreTokens().not()) _tokenizer = StringTokenizer(_reader.readLine() ?: return "", " ")
+    return _tokenizer.nextToken()
+}
+
+fun readInt() = _read().toInt()
+fun readDouble() = _read().toDouble()
+fun readLong() = _read().toLong()
+fun readString() = _read()
+fun readVString(n: Int) = List(n) { _read() }
+fun readLines(n: Int) = List(n) { readLn() }
+fun readVInt(n: Int) = List(n) { _read().toInt() }
+fun readAInt(n: Int) = IntArray(n) { _read().toInt() }
+fun readVDouble(n: Int) = List(n) { _read().toDouble() }
+fun readADouble(n: Int) = DoubleArray(n) { _read().toDouble() }
+fun readVLong(n: Int) = List(n) { _read().toLong() }
+fun readALong(n: Int) = LongArray(n) { _read().toLong() }
+fun read(d: Int) = readInt() - d
+fun read(d: Long) = readInt() - d
+fun readV(n: Int, d: Int) = readVInt(n).map { x -> x - d }
+fun readV(n: Int, d: Long) = readVLong(n).map { x -> x - d }
+fun readA(n: Int, d: Int) = readAInt(n).map { x -> x - d }
+fun readA(n: Int, d: Long) = readALong(n).map { x -> x - d }
+
+@JvmField
+val _writer = PrintWriter(OUTPUT, false)
+inline fun output(block: PrintWriter.() -> Unit) {
+    _writer.apply(block).flush()
+}
+
+// old
+//fun next() = readLine()!!
+//fun nint() = next().toInt()
+//fun nlong() = next().toLong()
+//fun ndouble() = next().toDouble()
+//fun nlstring() = next().split(" ")
+//fun nlint() = nlstring().map { it.toInt() }
+//fun nllong() = nlstring().map { it.toLong() }
+//fun nldouble() = nlstring().map { it.toDouble() }
+//fun reads() = next()
+//fun readi() = nint()
+//fun readl() = nlong()
+//fun readd() = ndouble()
+//fun readVs() = nlstring()
+//fun readVi() = nlint()
+//fun readVl() = nllong()
+//fun readVd() = nldouble()
+//
+//fun read() = readl()
+//fun readV() = readVl()
--- Template.kt
+++ Template.kt
@@ -1,22 +1,62 @@
-///In the name of ALLAH
+//In the name of ALLAH
 
-import java.lang.AssertionError
- 
-private fun readLn() = readLine()!! // string line
-private fun readInt() = readLn().toInt() // single int
-private fun readLong() = readLn().toLong() // single long
-private fun readDouble() = readLn().toDouble() // single double
-private fun readStrings() = readLn().split(" ") // list of strings
-private fun readInts() = readStrings().map { it.toInt() } // list of ints
-private fun readLongs() = readStrings().map { it.toLong() } // list of longs
-private fun readDoubles() = readStrings().map { it.toDouble() } // list of doubles
- 
-private fun myAssert(x: Boolean) {
-    if (!x) {
-        throw AssertionError()
-    }
+import java.io.PrintWriter
+import java.util.*
+import kotlin.collections.ArrayDeque
+
+val pw = PrintWriter(System.out)
+fun main() {
+    _writer.solve(); _writer.flush()
 }
 
-fun main(args: Array<String>) {
+fun PrintWriter.solve() {
+    var n = readInt();
+    var a = IntArray(5);
+    a = readAInt ( n );
     
+    for (i in 0..n-1) println(a[i]);
 }
+
+@JvmField
+val INPUT = System.`in`
+
+@JvmField
+val OUTPUT = System.out
+
+@JvmField
+val _reader = INPUT.bufferedReader()
+fun readLine(): String? = _reader.readLine()
+fun readLn() = _reader.readLine()!!
+
+@JvmField
+var _tokenizer: StringTokenizer = StringTokenizer("")
+fun _read(): String {
+    while (_tokenizer.hasMoreTokens().not()) _tokenizer = StringTokenizer(_reader.readLine() ?: return "", " ")
+    return _tokenizer.nextToken()
+}
+
+fun readInt() = _read().toInt()
+fun readDouble() = _read().toDouble()
+fun readLong() = _read().toLong()
+fun readString() = _read()
+fun readVString(n: Int) = List(n) { _read() }
+fun readLines(n: Int) = List(n) { readLn() }
+fun readVInt(n: Int) = List(n) { _read().toInt() }
+fun readAInt(n: Int) = IntArray(n) { _read().toInt() }
+fun readVDouble(n: Int) = List(n) { _read().toDouble() }
+fun readADouble(n: Int) = DoubleArray(n) { _read().toDouble() }
+fun readVLong(n: Int) = List(n) { _read().toLong() }
+fun readALong(n: Int) = LongArray(n) { _read().toLong() }
+fun read(d: Int) = readInt() - d
+fun read(d: Long) = readInt() - d
+fun readV(n: Int, d: Int) = readVInt(n).map { x -> x - d }
+fun readV(n: Int, d: Long) = readVLong(n).map { x -> x - d }
+fun readA(n: Int, d: Int) = readAInt(n).map { x -> x - d }
+fun readA(n: Int, d: Long) = readALong(n).map { x -> x - d }
+
+@JvmField
+val _writer = PrintWriter(OUTPUT, false)
+inline fun output(block: PrintWriter.() -> Unit) {
+    _writer.apply(block).flush()
+}
+
